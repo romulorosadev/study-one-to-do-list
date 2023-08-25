d1178e9d4f0e3a329bad7c8d931a414a34d23562
Romulo Rosa | @romulorosaev <romulorosadev@gmail.com>
2023-08-25 15:47:31 -0300

    js acessando elementos

---
+++ projeto_inicial/assets/js/app.js
@@ -0,0 +1,16 @@
+// 1 - Acessando os elementos e declarando em variavéis
+
+
+//1
+const inCampoTexto = document.querySelector('.form-input');
+
+const btNovoItem = document.querySelector('.form-button');
+
+let inList = document.querySelector('.list');
+
+let tarefa = document.querySelector('.task');
+
+let tarefaTexto = document.querySelector('.content');
+
+let tarefaConteudo = tarefaTexto.textContent;
+console.log(tarefaConteudo);
